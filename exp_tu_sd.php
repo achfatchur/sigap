2cdb3c8353ce71c2c9d33f349964acfce96bb6bc
achfatchur <frachmad712@gmail.com>
2023-02-28 17:31:35 +0700

    update sore

--- exp_tu_sd.php
+++ exp_tu_sd.php
@@ -6,7 +6,7 @@ require_once'vendor/tbs_class.php';
 require_once'vendor/tbs_plugin_opentbs.php';
 $TBS = new clsTinyButStrong; 
 $TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN);
-$sql = mysqli_query($con, "SELECT gaji_tu_sd.pegawai, gaji_tu_sd.tahun, pegawai.nama, gaji_tu_sd.total,bulan.bulan, gaji_tu_sd.pid FROM gaji_tu_sd INNER JOIN pegawai ON gaji_tu_sd.pegawai = pegawai.nip INNER JOIN bulan ON gaji_tu_sd.bulan = bulan.id order by gaji_tu_sd.pid DESC");	
+$sql = mysqli_query($con, "SELECT gaji_tu_sd.pegawai, gaji_tu_sd.tahun, pegawai.nama, gaji_tu_sd.total,bulan.bulan, gaji_tu_sd.pid FROM gaji_tu_sd INNER JOIN pegawai ON gaji_tu_sd.pegawai = pegawai.nip INNER JOIN bulan ON gaji_tu_sd.bulan = bulan.id");	
 $data = [];
 while($row = mysqli_fetch_array($sql))
 {     
